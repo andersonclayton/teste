d5f37d73d306722a42f35792d05265321a5ac94c
anderson <anderson@sti.ufpb.br>
2016-08-01 18:33:55 -0300

    Atualiza teste SO.

--- Teste/src/DiscoveryOperationalSystem.java
+++ Teste/src/DiscoveryOperationalSystem.java
@@ -2,8 +2,12 @@
 public class DiscoveryOperationalSystem {
 
 	public static void main(String[] args) {
-		System.out.println(System.getProperties());
-		System.out.println(System.getProperty("os.name"));
+//		System.out.println(System.getProperties());
+//		System.out.println(System.getProperty("os.name"));
+//		System.getProperties().list(System.out);
+		if(System.getProperty("os.name").contentEquals("%linux%")){
+			System.out.println("É linux");
+		}
 	}
 
 }
