c54767add2b2125107eb9daa2bc13c3c6eed4f6b
anderson <anderson@sti.ufpb.br>
2016-08-01 17:04:18 -0300

    Descobrindo o Sistema Operacional

---
+++ Teste/src/DiscoveryOperationalSystem.java
@@ -0,0 +1,9 @@
+
+public class DiscoveryOperationalSystem {
+
+	public static void main(String[] args) {
+		System.out.println(System.getProperties());
+		System.out.println(System.getProperty("os.name"));
+	}
+
+}
